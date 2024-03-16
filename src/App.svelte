e5d06b6e7834260b20971302313b31e001de6882
Brettell <thomas.a.brettell@gmail.com>
2024-03-16 18:08:33 +1100

    init

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  let percentHeigt;
+  let vhHeight;
+  let dvhHeight;
+  let lvhHeight;
+  let svhHeight;
+</script>
+
+<div bind:clientHeight={percentHeigt} class="sizer percent" />
+<div bind:clientHeight={vhHeight} class="sizer vh" />
+<div bind:clientHeight={dvhHeight} class="sizer dvh" />
+<div bind:clientHeight={lvhHeight} class="sizer lvh" />
+<div bind:clientHeight={svhHeight} class="sizer svh" />
+
+<p>100% = {percentHeigt}</p>
+<p>100vh = {vhHeight}</p>
+<p>100dvh = {dvhHeight}</p>
+<p>100lvh = {lvhHeight}</p>
+<p>100svh = {svhHeight}</p>
+
+<style>
+  p {
+    margin: 0;
+  }
+
+  .sizer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    user-select: none;
+    pointer-events: none;
+    opacity: 0;
+  }
+
+  .sizer.percent {
+    height: 100%;
+  }
+
+  .sizer.vh {
+    height: 100vh;
+  }
+
+  .sizer.dvh {
+    height: 100dvh;
+  }
+
+  .sizer.lvh {
+    height: 100lvh;
+  }
+
+  .sizer.svh {
+    height: 100svh;
+  }
+</style>
